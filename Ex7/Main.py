23b79b00d425c597685b9a0639fd41a4aa2ec216
Knowledge Shelf <vishwajeetsinghrana8@gmail.com>
2020-03-11 12:38:06 +0530

    Add files via upload

---
+++ Ex7/Main.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField,SubmitField
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'knowledgeshelf'
+
+class InfoForm(FlaskForm):
+    breed = StringField('What type of breed are you??')
+    submit = SubmitField('Submit')
+
+@app.route('/', methods=['GET','POST'])
+def index():
+    breed = False
+    form = InfoForm()
+    if form.validate_on_submit():
+        breed = form.breed.data
+        form.breed.data=''
+    return render_template('base.html', form=form, breed=breed)
+
+if __name__ == '__main__':
+    app.run(debug=True)
